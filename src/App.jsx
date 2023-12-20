a70e730bc73dcbf6fffcf2529e26e74d6caa972c
Christian Fernandez <christian14@gmail.com>
2023-12-20 20:27:22 -0300

    Cargando el state de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react";
 
+const array = [
+  {name:'Tomate', price: 1500, imagen:'../public/productos/tomate.jpg'},
+  {name:'Lechuga', price: 500, imagen:'../public/productos/lechuga.jpg'},
+  {name:'Arbejas', price: 2500, imagen:'../public/productos/arbejas.jpg'},
+
+]
 const App = () => {
+
+  const [productos, setProductos] = useState([])
+
+  useEffect(()=>{
+    const cargandoProdutos = ()=> {
+      setProductos(array)
+    }
+    cargandoProdutos()
+  },[])
+
   return (
     <>
       <h1>Carrito de compras</h1>
